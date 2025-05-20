ef9125e1861d58f098014989c4625293931379af
Cláudio Cardoso <claudio.c.lima@hotmail.com>
2025-05-20 18:49:55 -0300

    first commit

---
+++ backend/src/main/java/br/com/surb/project_dslist/ProjectDslistApplication.java
@@ -0,0 +1,13 @@
+package br.com.surb.project_dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDslistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/surb/project_dslist/ProjectDslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.surb.project_dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
